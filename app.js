cd7c675696bafa15327b69387a3ac580b9946bfe
PatelJenishp <jenishppatel23@gnu.ac.in>
2026-02-05 13:15:14 +0530

    Fix typo in response message my secind verison

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by jenishpatel to test pipeline and this is the second version');
+    res.send('Hello, this is a code by jenishpatel to test pipeline and this is the  version second version');
   });
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
